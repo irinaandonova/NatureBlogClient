9e3300ce6a0b7038a83bb11af3fb7097c636da06
irinaandonova <irinaandonova97@gmail.com>
2023-01-23 17:55:41 +0200

    Configure react app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { PublicClientApplication } from '@azure/msal-browser';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
+
+const pca = new PublicClientApplication({
+  auth: {
+    clientId: 'a7d91e71-ae1d-4347-9f2e-249dd5a8270e',
+    redirectUri: '/',
+    authority: 'https://login.microsoftonline.com/common'
+  }
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App msauInstance={pca}/>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
