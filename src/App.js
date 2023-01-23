25706a6f7fd636a82ad5632cfc1cb757048cd468
irinaandonova <irinaandonova97@gmail.com>
2023-01-23 17:54:41 +0200

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Header from "./pages/Header/Header";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Details from "./pages/Details/Details";
+import CreateDestination from "./pages/CreateDestination/CreateDestination";
+import EditDestination from "./pages/EditDestination/EditDestination";
+import { AuthContextProvider } from "./auth/authContext";
+import { CommentsStore } from "./reducers/CommentsReducer";
+import { MsalProvider } from '@azure/msal-react';
+import ButtonAppBar from "./components/Menu/Menu";
+import PersistentDrawerLeft from "./components/SideMenu/SideMenu";
 
-function App() {
+function App({ msauInstance }) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MsalProvider instance={msauInstance}>
+        <AuthContextProvider>
+          <ButtonAppBar />
+          <PersistentDrawerLeft />
+          <CommentsStore>
+            <Routes>
+              <Route path='/' element={<Dashboard />} />
+              <Route path='/:destinationType' element={<Dashboard />} />
+              <Route path='/destination/:id' element={<Details />} />
+              <Route path='/destination/create' element={<CreateDestination />} />
+              <Route path='/destination/:id/edit' element={<EditDestination />} />
+            </Routes>
+          </CommentsStore>
+        </AuthContextProvider>
+      </MsalProvider>
     </div>
   );
 }
